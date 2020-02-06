753ce3f9ccf45417774bdfbed8ac46d81c28a492
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-02-06 13:46:58 -0800

    Add Blopper Model This model extends the user class.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Blopper(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  # liked_blops = 
+  # my_blops = 
+  @receiver(post_save, sender=User)
+  def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+      Blopper.objects.create(user=instance)
+  @receiver(post_save, sender=User)
+  def save_user_profile(sender, instance, **kwargs):
+    instance.blopper.save()
