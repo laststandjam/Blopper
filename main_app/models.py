d945df6e4b263ae0c0297ba5db79622d25561cf2
ihavehands <zaneshook@gmail.com>
2020-02-06 12:33:50 -0800

    add comments

--- main_app/models.py
+++ main_app/models.py
@@ -7,6 +7,11 @@ class Blop(models.Model):
     content  = models.URLField(max_length=200)
     article = models.TextField(max_length="1000")
     likes = models.PositiveIntegerField()
-    created_at = models.DateTimeField test(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comments(models.Model):
+    creator = models.ForeignKey(User, on_delete =models.CASCADE)
+    content = models.TextField(max_length= 300)
+    created_at = models.DateTimeField(auto_now_add=True)
     
     
