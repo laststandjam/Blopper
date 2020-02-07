007a477931884fd2c7b59282e118b49fe91657fb
ihavehands <zaneshook@gmail.com>
2020-02-06 16:20:16 -0800

    upadate blop model, change content to video, add image key/value

--- main_app/models.py
+++ main_app/models.py
@@ -18,7 +18,8 @@ class Blopper(models.Model):
 class Blop(models.Model):
   creator = models.ForeignKey(User, on_delete =models.CASCADE)
   title = models.CharField (max_length=100)
-  content  = models.URLField(max_length=200)
+  video  = models.URLField(max_length=200)
+  image = models.ImageField(width_field=400, height_field=400)
   article = models.TextField(max_length="1000")
   likes = models.PositiveIntegerField()
   created_at = models.DateTimeField(auto_now_add=True)
