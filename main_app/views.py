21ed8355551809a83f2821e65b64710770828926
ihavehands <zaneshook@gmail.com>
2020-02-07 11:33:02 -0800

    working on home view/template

--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from .models import Blop
 
 def home(request):
-  return render(request, 'main_app/home.html')
+  videos = Blop.objects.filter(video != None)
+  pictures = Blop.objects.filter(image != None)
+  articles = Blop.objects.filter(article != None)
+  return render(request, 'main_app/home.html',{'videos': videos,
+   'pictures':pictures,
+   'articles':articles,
+   })
 
 def signup(request):
   error_message = ''
@@ -13,9 +20,18 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('main_app:index')
+      return redirect('main_app:home')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+class BlopCreate(CreateView):
+  #add something that checks if a user is logged in.
+  #if user is not logged in they are redirected to the login page
+  model = Blop
+  fields = ['title', 'video', 'image', 'article']
+  def form_valid(self, form):
+    form.instance.creator = self.request.user
+    return super().form_valid(form)
