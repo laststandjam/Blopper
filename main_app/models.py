3f2d0ff62438b5028a414c0bb7e2f813a9569404
Sophia Sanh <Sophiasanhbizcoach@gmail.com>
2020-02-07 17:27:45 -0800

    Jeff: working commit

--- main_app/models.py
+++ main_app/models.py
@@ -8,7 +8,7 @@ class Blop(models.Model):
   creator = models.ForeignKey(User, on_delete =models.CASCADE)
   title = models.CharField (max_length=100)
   video  = models.URLField(max_length=200, null=True, blank=True)
-  image = models.ImageField(null=True, blank=True)
+  image = models.URLField(max_length=200, null=True, blank=True)
   article = models.TextField(max_length="1000", blank=True)
   likes = models.PositiveIntegerField(default=0)
   created_at = models.DateTimeField(auto_now_add=True)
