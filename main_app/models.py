f88972436705ea3acc348069ce417fc9536e31b9
ihavehands <zaneshook@gmail.com>
2020-02-08 10:09:42 -0800

    working on image display

--- main_app/models.py
+++ main_app/models.py
@@ -7,8 +7,8 @@ from django.urls import reverse
 class Blop(models.Model):
   creator = models.ForeignKey(User, on_delete =models.CASCADE)
   title = models.CharField (max_length=100)
-  video  = models.URLField(max_length=200, null=True, blank=True)
-  image = models.ImageField(null=True, blank=True)
+  video  = models.URLField(max_length=1000, null=True, blank=True)
+  image = models.URLField(max_length=1000, null=True, blank=True)
   article = models.TextField(max_length="1000", blank=True)
   likes = models.PositiveIntegerField(default=0)
   created_at = models.DateTimeField(auto_now_add=True)
@@ -27,7 +27,7 @@ class Blopper(models.Model):
   @receiver(post_save, sender=User)
   def save_user_profile(sender, instance, **kwargs):
     instance.blopper.save()
-    # class Meta
+
     
     
 
